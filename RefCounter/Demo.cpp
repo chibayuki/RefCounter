54a70cb6e1ea66007ce60bb5ebae3125b2d9cff5
千夏茹初 <chibayuki@foxmail.com>
2019-10-21 05:59:26 +0800

    Add files via upload

---
+++ RefCounter/Demo.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+#include "RefCounter.h"
+
+using namespace std;
+
+#define bool_to_str(val) ((val) ? ("true") : ("false"))
+
+int main()
+{
+	Ref<int> r1 = 42; // r1是指向值为42的int对象的引用
+	Ref<int> r2 = r1; // 使r2与r1指向同一个int对象
+	Ref<int> r3 = *r1; // r3指向r1所指对象的浅表副本
+
+	cout << "r1 = " << r1 << endl; // Output: 42
+	cout << "r2 = " << r2 << endl; // Output: 42
+	cout << "r3 = " << r3 << endl; // Output: 42
+	cout << "r1 == r2: " << bool_to_str(r1 == r2) << endl; // Output: true
+	cout << "r1 == r3: " << bool_to_str(r1 == r3) << endl; // Output: false
+	cout << endl;
+
+	*r1 = r3 / 2; // 更改r1所指对象的值
+
+	cout << "r1 = " << r1 << endl; // Output: 21
+	cout << "r2 = " << r2 << endl; // Output: 21
+	cout << "r3 = " << r3 << endl; // Output: 42
+	cout << endl;
+
+	r1 = 1024; // 使r1指向新的对象
+
+	cout << "r1 = " << r1 << endl; // Output: 1024
+	cout << "r2 = " << r2 << endl; // Output: 21
+	cout << "r3 = " << r3 << endl; // Output: 42
+	cout << endl;
+}
---
+++ RefCounter/RefCounter.h
@@ -0,0 +1,133 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+Copyright © 2019 chibayuki@foxmail.com
+
+RefCounter
+Version 19.10.21.0000
+
+This file is part of RefCounter
+
+RefCounter is released under the GPLv3 license
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#pragma once
+
+// 支持引用计数与垃圾回收的指针
+template <typename T> class Ref
+{
+private:
+	T* _Ptr;
+	size_t* _Count;
+
+	inline void _Increase()
+	{
+		if (_Ptr)
+		{
+			(*_Count)++;
+		}
+	}
+
+	inline void _Decrease()
+	{
+		if (_Ptr)
+		{
+			(*_Count)--;
+
+			if (*_Count == 0)
+			{
+				delete _Ptr;
+				delete _Count;
+			}
+		}
+	}
+
+	Ref()
+	{
+		_Ptr = nullptr;
+		_Count = new size_t(0);
+	}
+
+public:
+	Ref(const T* ptr = nullptr)
+	{
+		_Ptr = ptr;
+		_Count = new size_t(_Ptr ? 1 : 0);
+	}
+
+	Ref(const T& val)
+	{
+		_Ptr = new T();
+		_Count = new size_t(_Ptr ? 1 : 0);
+
+		*_Ptr = val;
+	}
+
+	Ref(const Ref& ref)
+	{
+		_Ptr = ref._Ptr;
+		_Count = ref._Count;
+
+		_Increase();
+	}
+
+	Ref& operator=(const Ref& ref)
+	{
+		if (this == &ref)
+		{
+			return *this;
+		}
+
+		if (_Ptr == ref._Ptr)
+		{
+			return *this;
+		}
+
+		_Decrease();
+
+		_Ptr = ref._Ptr;
+		_Count = ref._Count;
+
+		_Increase();
+
+		return *this;
+	}
+
+	~Ref()
+	{
+		_Decrease();
+	}
+
+	inline bool operator==(const Ref& ref) const
+	{
+		return (_Ptr == ref._Ptr);
+	}
+
+	inline bool operator!=(const Ref& ref) const
+	{
+		return (_Ptr != ref._Ptr);
+	}
+
+	inline T& operator*()
+	{
+		return *_Ptr;
+	}
+
+	inline const T& operator*() const
+	{
+		return *_Ptr;
+	}
+
+	inline T* operator->()
+	{
+		return _Ptr;
+	}
+
+	inline const T* operator->() const
+	{
+		return _Ptr;
+	}
+
+	inline operator T() const
+	{
+		return *_Ptr;
+	}
+};
